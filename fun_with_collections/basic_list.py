c491b43da0cbc2c0e89e8974ab32d91b60e1fd11
chasevanblair <crvanblair@dmacc.edu>
2020-06-19 13:17:45 -0500

    Initial commit

---
+++ fun_with_collections/basic_list.py
@@ -0,0 +1,8 @@
+def make_list():
+    """return list of user input"""
+    pass
+
+
+def get_input():
+    """gets one user input then returns it"""
+    pass
---
+++ test_collections/test_basic_list.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest.mock import patch
+
+from fun_with_collections import basic_list
+
+
+class TestList(unittest.TestCase):
+    @patch('fun_with_collections.basic_list.get_input', return_value='5')
+    def test_make_list(self, user_in):
+        self.assertEqual(basic_list.make_list(), [2, 2, 2])
